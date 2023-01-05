cb1909eb63e8f3cd1072decbffe1d22f64d22edb
Farman Ali <farman.a@servify.in>
2023-01-05 10:43:39 +0530

    averagePair and isSubsequence problem solved

---
+++ isSubsequence.js
@@ -0,0 +1,32 @@
+/*
+Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+Examples:
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+
+*/
+
+const isSubsequence = (str1,str2)=>{
+    if(!str1.length)
+        return false 
+
+    let i = 0
+    let j = 0
+
+    while(j < str2.length){
+        if(str2[j] === str1[i]){
+            i++
+        }
+        if(i === str1.length){
+            return true
+        }
+        j++
+    }
+    return false
+}
+
+console.log(isSubsequence('abc', 'acb'))
